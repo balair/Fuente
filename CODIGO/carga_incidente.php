65857097ac29d4c7cf4f3276e9339634a2b2291c
balair <dante.furcigianni@gmail.com>
2024-09-13 17:45:53 -0300

    v0.02 inclusion de carga incidente

---
+++ CODIGO/carga_incidente.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <title>Project_v.1</title>
+</head>
+<body>
+    <div class="form-data-incident">
+    	<div class="form-data-f1">
+    		<label for="" class="label-i-1">Nombre de Usuario: </label>
+        	<!--<input type="text" id="usuario" name="usuario" class="input-form-incident">-->
+        	<label for="" class="label-i-2">Correo: </label>
+        	<!--<input type="text" id="area" name="area" class="input-form-incident">-->
+        	<label for="" class="label-i-3">Departamento: </label>
+
+        	<label for="" class="label-i-4">Ubicacion: </label>
+
+        	<label for="" class="label-i-5">Telefono: </label>
+
+        	<select id="adm" name="adm" class="input-form-incident-n"></select>
+        	<label for="" class="input-form-incident-c">prueba@prueba.com </label>
+        	<label for="" class="input-form-incident-d">Prueba </label>
+        	<label for="" class="input-form-incident-u">Ubi prueba </label>
+        	<label for="" class="input-form-incident-t">288 </label>
+    	</div>
+    	<div class="form-data-f2">
+    		<label for="" class="label-i-1">Asignar a: </label>
+    		<label for="" class="label-i-2">Categoria: </label>
+    		<label for="" class="label-i-3">Estado: </label>
+    		<label for="" class="label-i-4">Tipo: </label>
+    		<label for="" class="label-i-5">Llamados: </label>
+
+    		<select id="adm" name="adm" class="input-form-incident-n"></select>
+    		<select id="" name="" class="input-form-incident-c"></select>
+    		<select id="" name="" class="input-form-incident-d"></select>
+    		<select id="" name="" class="input-form-incident-u"></select>
+    		<input type="checkbox" name="grabar llamados">
+    	</div>
+    </div>
+</body>
+</html>
